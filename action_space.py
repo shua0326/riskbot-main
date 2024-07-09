8e33891cc58bdf039b2449aeb061c11732189a08
shua0326 <shuyanghua123@gmail.com>
2024-07-09 20:29:50 +1000

    updated action space
    changed action space to generate a tensor of possible actions for claim territory and place initial troop query cases

--- action_space.py
+++ action_space.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+
 """
 match query:
     case QueryClaimTerritory() as 1:
@@ -18,6 +20,17 @@ match query:
     case QueryFortify() as 8:
 """
 
+query_type_mapping = {
+    "ClaimTerritory": 0,
+    "PlaceInitialTroop": 1,
+    "RedeemCards": 2,
+    "DistributeTroops": 3,
+    "Attack": 4,
+    "TroopsAfterAttack": 5,
+    "Defend": 6,
+    "Fortify": 7
+}
+
 class ActionSpace:
     def __init__(self):
         self.query = ""
@@ -33,15 +46,41 @@ class ActionSpace:
         self.cards = cards
         self.total_troops = total_troops
 
-    def choose_random_action(self):
+    #need to change this so that a list/tensor of actions is constructed first
+    def create_action_space(self):
         match self.query:
             case "ClaimTerritory":
-                chosen_territory_to_claim = np.random.choice(len(self.adjacent_territories))
-                return chosen_territory_to_claim
+                num_query_types = 8
+                num_territories = 42
+                actions_tensor = torch.zeros((num_query_types, num_territories))
+
+                # Encode the query type
+                query_type_index = query_type_mapping[self.query]
+                for action in self.adjacent_territories:
+                    # One-hot encode the territory ID
+                    actions_tensor[query_type_index, action.id] = 1
+
+                actions_tensor = actions_tensor.unsqueeze(0).float()
+
+                return actions_tensor
+
             case "PlaceInitialTroop":
-                chosen_territory_to_place_troop = np.random.choice(len(self.my_territories))
-                troops_to_place = np.random.randint(1, self.total_troops + 1)
-                return chosen_territory_to_place_troop, troops_to_place
+                num_query_types = len(query_type_mapping)
+                num_territories = 42
+                max_troops = self.total_troops
+                # Initialize the tensor with an extra dimension for troop counts
+                actions_tensor = torch.zeros((num_query_types, num_territories, max_troops))
+
+                query_type_index = query_type_mapping[self.query]
+                for territory in self.my_territories:
+                    for troop_count in range(1, max_troops + 1):
+                        # Encode the possibility of placing each number of troops in each territory
+                        actions_tensor[query_type_index, territory.id, troop_count - 1] = 1
+
+                actions_tensor = actions_tensor.unsqueeze(0).float()
+
+                return actions_tensor
+            
             case "RedeemCards":
                 return self.redeem_cards()
             case "DistributeTroops":
